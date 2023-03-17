798376995ae4741e6c01a13e8362597cc8c738d1
huang0520 <huangmorris4x@gmail.com>
2023-03-18 02:06:52 +0800

    graph_representation_v2 update

--- Lab3/graph_representation_v2.cpp
+++ Lab3/graph_representation_v2.cpp
@@ -1,33 +1,33 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-class graph{
-public:
-    graph(int num_node){
-        this->edge_list = vector <vector <int>>(num_node);
+class graph {
+   public:
+    graph(int num_node) { this->edge_list = vector<vector<int>>(num_node); }
+
+    int degree(int u) { return this->edge_list[u - 1].size(); }
+
+    auto neighbor(int u, int v) -> char {
+        auto& i = this->edge_list[u - 1];
+        if (find(i.begin(), i.end(), v) != i.end())
+            return 'Y';
+        else
+            return 'N';
     }
 
-    int degree(int u) {return this->edge_list[u - 1].size();}
-
-    char nieghbor(int u, int v){
-        auto &i = this->edge_list[u - 1];
-        if(find(i.begin(), i.end(), v) != i.end()) return 'Y';
-        else return 'N';
-    }
-
-    void connect(int u, int v){
+    void connect(int u, int v) {
         this->edge_list[u - 1].emplace_back(v);
         this->edge_list[v - 1].emplace_back(u);
     }
 
-private:
-    vector <vector <int>> edge_list;
+   private:
+    vector<vector<int>> edge_list;
 };
 
-int main(void){
+int main(void) {
     // ios optimization
     ios::sync_with_stdio(false);
     cin.tie(0);
@@ -36,7 +36,7 @@ int main(void){
     int num_node, num_edge, num_query;
     cin >> num_node >> num_edge >> num_query;
 
-    // Construct the adjency list
+    // Construct the adjacency list
     graph G(num_node);
 
     // Connect the node
@@ -51,17 +51,16 @@ int main(void){
         char op;
         cin >> op;
 
-        if (op == 'D'){
+        if (op == 'D') {
             int val;
             cin >> val;
 
             cout << G.degree(val) << '\n';
-        }
-        else if (op == 'N'){
+        } else if (op == 'N') {
             int u, v;
             cin >> u >> v;
 
-            cout << G.nieghbor(u, v) << '\n';
+            cout << G.neighbor(u, v) << '\n';
         }
     }
 }
