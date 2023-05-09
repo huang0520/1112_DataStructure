e73d18c0114197e0ea51549e1469f0b664b9510f
huang0520 <huangmorris4x@gmail.com>
2023-05-09 22:59:14 +0800

    Lab 8 PA

--- Lab8/integer_ordered_set.cpp
+++ Lab8/integer_ordered_set.cpp
@@ -7,11 +7,10 @@ using namespace std;
 class BST {
     struct Node {
         int val{};
-        shared_ptr<Node> parent;
         unique_ptr<Node> left;
         unique_ptr<Node> right;
 
-        Node(int n) : val{n}, left{nullptr}, right{nullptr}, parent{nullptr} {}
+        Node(int n) : val{n}, left{nullptr}, right{nullptr} {}
     };
 
     unique_ptr<Node> root{nullptr};
@@ -21,32 +20,88 @@ class BST {
     BST() {}
 
     auto ins(int n) -> void {
-        auto &pos = search(n);
-        if (pos == nullptr) pos = make_unique<Node>(n);
-
-        size++;
+        auto &pos{search(n)};
+        if (not pos) {
+            pos = make_unique<Node>(n);
+            size++;
+        }
     }
 
-    auto del(int n) -> void {}
+    auto del(int n) -> void {
+        if (del_helper(n, root)) size--;
+    }
 
     auto search(int n) -> unique_ptr<Node> & { return search_helper(n, root); }
 
-    auto lower_bound(int n) -> int { /* code */
+    auto lower_bound(int n) -> int {
+        auto curr{root.get()};
+        int lb{-1};
+
+        while (curr) {
+            if (n < curr->val) {
+                lb = lb == -1 ? curr->val : min(curr->val, lb);
+                curr = curr->left.get();
+            } else if (n > curr->val) {
+                curr = curr->right.get();
+            } else {
+                lb = curr->val;
+                break;
+            }
+        }
+        return lb;
     }
 
-    auto upper_bound(int n) -> int { /* code */
+    auto upper_bound(int n) -> int {
+        auto curr{root.get()};
+        int ub{-1};
+
+        while (curr) {
+            if (n < curr->val) {
+                ub = ub == -1 ? curr->val : min(curr->val, ub);
+                curr = curr->left.get();
+            } else if (n >= curr->val) {
+                curr = curr->right.get();
+            }
+        }
+        return ub;
     }
 
-    auto size() -> int { return size; }
+    auto get_size() -> int { return size; }
 
    private:
-    auto search_helper(int n, unique_ptr<Node> &ptr) -> unique_ptr<Node> & {
-        if (ptr == nullptr or n == ptr->val)
-            return ptr;
-        else if (n > ptr->val)
-            return search_helper(n, ptr->right);
+    auto search_helper(int n, unique_ptr<Node> &node) -> unique_ptr<Node> & {
+        if (node == nullptr or n == node->val)
+            return node;
+        else {
+            return search_helper(n, n < node->val ? node->left : node->right);
+        }
+    }
+
+    auto del_helper(int n, unique_ptr<Node> &node) -> bool {
+        // not found the node that has value n
+        if (not node) return false;
+
+        if (n != node->val)
+            return del_helper(n, n < node->val ? node->left : node->right);
+
+        // node has no child or only right
+        if (not node->left) node = std::move(node->right);
+
+        // node has 2 child or only left
+        else {
+            auto &sub_max{find_max(node->left)};
+            sub_max->right = std::move(node->right);
+            node = std::move(sub_max);
+        }
+
+        return true;
+    }
+
+    auto find_max(unique_ptr<Node> &node) -> unique_ptr<Node> & {
+        if (node->right)
+            return find_max(node->right);
         else
-            return search_helper(n, ptr->left);
+            return node;
     }
 };
 
@@ -56,7 +111,37 @@ auto main() -> int {
 
     BST bst{};
 
-    if (!bst.search(5)) cout << "NO\n";
+    while (num_op--) {
+        char op;
+        int val;
+        cin >> op >> val;
+
+        if (op == 'I') {
+            bst.ins(val);
+            cout << bst.get_size() << "\n";
+        }
+
+        else if (op == 'D') {
+            bst.del(val);
+            cout << bst.get_size() << "\n";
+        }
+
+        else if (op == 'S') {
+            auto &node = bst.search(val);
+            if (not node)
+                cout << "NO\n";
+            else
+                cout << "YES\n";
+        }
+
+        else if (op == 'L') {
+            cout << bst.lower_bound(val) << "\n";
+        }
+
+        else if (op == 'U') {
+            cout << bst.upper_bound(val) << "\n";
+        }
+    }
 
     return 0;
 }
