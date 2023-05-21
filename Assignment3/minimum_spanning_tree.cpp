82f48dce2a0e094591c76da2673612731c8d3483
huang0520 <huangmorris4x@gmail.com>
2023-05-21 15:16:50 +0800

    MST finished

--- Assignment3/minimum_spanning_tree.cpp
+++ Assignment3/minimum_spanning_tree.cpp
@@ -1,54 +1,68 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-struct Graph {
-    struct Node {
-        size_t idx;
-        vector<pair<shared_ptr<Node>, size_t>> adj;
+struct DST {
+    vector<size_t> parent;
+    vector<size_t> group_size;
 
-        Node(size_t n) : idx{n} {}
-    };
-
-    vector<shared_ptr<Node>> node_ptr;
-
-    Graph(size_t n) {
-        node_ptr.reserve(n);
-        for (size_t i = 0; i < n; i++)
-            node_ptr.emplace_back(make_shared<Node>(i));
+    DST(size_t num_node) : group_size(num_node, 1) {
+        parent.reserve(num_node);
+        for (size_t i = 0; i < num_node; i++) parent.emplace_back(i);
     }
 
-    auto connect(size_t u, size_t v, size_t w) -> void {
-        auto &U = node_ptr[u];
-        auto &V = node_ptr[v];
-
-        U->adj.emplace_back(V, w);
-        V->adj.emplace_back(U, w);
+    auto find(size_t n) -> size_t {
+        return n == parent[n] ? n : find(parent[n]);
     }
 
-    auto display() -> void {
-        for (auto ptr : node_ptr) {
-            cout << "Node: " << ptr->idx + 1 << "\n";
-            for (auto edge : ptr->adj)
-                cout << "  " << edge.first->idx + 1 << " " << edge.second
-                     << "\n";
-        }
+    auto merge(size_t u, size_t v) -> bool {
+        auto root_u = find(u), root_v = find(v);
+
+        if (root_u == root_v) return false;
+
+        // root_u must the root of larger group
+        if (group_size[root_u] < group_size[root_v]) swap(root_u, root_v);
+        parent[root_v] = root_u;
+
+        group_size[root_u] += group_size[root_v];
+        return true;
     }
 };
+
+struct Edge {
+    size_t u, v, w;
+    Edge(size_t u, size_t v, size_t w) : u{u}, v{v}, w{w} {}
+};
+
+auto edge_less(Edge e1, Edge e2) -> bool { return e1.w < e2.w; }
 
 auto main() -> int {
     size_t num_node, num_edge;
     cin >> num_node >> num_edge;
 
-    Graph g(num_node);
+    DST g(num_node);
+    vector<Edge> edge;
+    edge.reserve(num_edge);
+    size_t total_weight{};
+
     // Connect graph
     while (num_edge--) {
         size_t u, v, w;
         cin >> u >> v >> w;
-        g.connect(u - 1, v - 1, w);
+
+        edge.emplace_back(u - 1, v - 1, w);
     }
 
-    g.display();
+    sort(edge.begin(), edge.end(), edge_less);
+
+    for (auto e : edge) {
+        auto [u, v, w] = e;
+        if (g.merge(u, v)) total_weight += w;
+    }
+
+    cout << total_weight << "\n";
+
     return 0;
 }
